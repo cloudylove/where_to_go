2b1edeb39b2511bcf20facb959f775ae2113eb9f
cloudylove <cloudylovechen@gmail.com>
2023-03-31 01:28:51 +0800

    載入地圖

---
+++ main.js
@@ -0,0 +1,5 @@
+var map = L.map('map').setView([22.6229557,120.2836755], 18);
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+}).addTo(map);
